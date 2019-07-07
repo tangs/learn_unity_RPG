71668913162a5983ef64735a91a9c04c80a62a5e
tangs <tangsong1988kaka@gmail.com>
2019-07-07 22:41:57 +0800

    update.

---
+++ Assets/Scripts/UIGameController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+
+public class UIGameController : MonoBehaviour
+{
+
+    void Start()
+    {
+
+    }
+
+    public void OnUpClicked()
+    {
+        
+    }
+
+    public void OnDownClicked()
+    {
+
+    }
+
+    public void OnLeftClicked()
+    {
+
+    }
+
+    public void OnRightClicked()
+    {
+        Debug.Log("r");
+    }
+}
